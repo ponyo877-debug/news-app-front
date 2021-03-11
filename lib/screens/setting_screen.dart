a61a3c8aa36ea33964715a2adac6995a1d52d7af
ponyo877-debug <hidakakis@nttdata.co.jp>
2021-03-12 00:11:17 +0900

    update setting screen style

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -14,50 +14,69 @@ class SettingScreen extends StatelessWidget {
       //debugShowCheckedModeBanner: false,
       //title: "SETTING",
       return Scaffold(
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.select_all),
-              title: Text(
-                'SELECT SITES',
-              ),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () async {
-                await Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => SelectSites()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email_outlined),
-              title: Text(
-                'CONTACT',
-              ),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                print(contactURL);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => MatomeWebView(
-                          title: "CONTACT",
-                          selectedUrl: contactURL,
-                        )));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.privacy_tip_outlined),
-              title: Text(
-                'PRIVACY POLICY',
-              ),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                print(PPURL);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => MatomeWebView(
-                          title: "PRIVACY POLICY",
-                          selectedUrl: PPURL,
-                        )));
-              },
-            ),
-          ],
+        body: ListView.separated(
+          itemCount: 3,
+          separatorBuilder:
+              (BuildContext context, int index) => Divider(
+                  color: Colors.white),
+          itemBuilder: (BuildContext context, int index) {
+            Widget listtile;
+            switch (index) {
+              case 0:
+                listtile = ListTile(
+                  leading: Icon(Icons.select_all),
+                  title: Text(
+                    'Select Site',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () async {
+                    await Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => SelectSites()));
+                  },
+                );
+                break;
+              case 1:
+                listtile = ListTile(
+                  leading: Icon(Icons.email_outlined),
+                  title: Text(
+                    'Contact Us',
+                      style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {
+                    print(contactURL);
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            MatomeWebView(
+                              title: "Contact Us",
+                              selectedUrl: contactURL,
+                            )));
+                  },
+                );
+                break;
+              case 2:
+                listtile = ListTile(
+                  leading: Icon(Icons.privacy_tip_outlined),
+                  title: Text(
+                    'Privacy Policy',
+                      style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {
+                    print(PPURL);
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            MatomeWebView(
+                              title: "Privacy Policy",
+                              selectedUrl: PPURL,
+                            )));
+                  },
+                );
+                break;
+            }
+            return listtile;
+          },
         ),
       );
   }
