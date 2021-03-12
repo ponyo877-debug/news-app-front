8382757da0c7e27829b3be15dff57bd271dc8a0e
ponyo877-debug <hidakakis@nttdata.co.jp>
2021-03-12 10:39:39 +0900

    modify setting_screen to make it reusable

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -2,82 +2,67 @@ import 'package:flutter/material.dart';
 import 'webview.dart';
 import 'select_sites.dart';
 
+class SettingInfo {
+  IconData icon;
+  String title;
+  Widget widget;
+  SettingInfo(this.icon, this.title, this.widget);
+}
+
 class SettingScreen extends StatelessWidget {
-  // final String contactURL = "https://forms.gle/PBs2r2YBbzzdQZd29";
-  final String contactURL =
-      "https://docs.google.com/forms/d/e/1FAIpQLSd-fuupDifDoJQ1uTkdyUCgzEiNvfUzdJe0YOhPfdSC3U2Erw/viewform?usp=sf_link";
-  final String PPURL = "http://gitouhon-juku-k8s2.ga/privacy_policy/";
+  SettingScreen._internal();
+  String _contactURL;
+  String _PPURL;
+  List<SettingInfo> _settingTabs;
+
+  factory SettingScreen() {
+    SettingScreen _settingScreen = SettingScreen._internal();
+    _settingScreen._contactURL =
+        "https://docs.google.com/forms/d/e/1FAIpQLSd-fuupDifDoJQ1uTkdyUCgzEiNvfUzdJe0YOhPfdSC3U2Erw/viewform?usp=sf_link";
+    _settingScreen._PPURL = "http://gitouhon-juku-k8s2.ga/privacy_policy/";
+
+    _settingScreen._settingTabs = [
+      SettingInfo(Icons.select_all, 'Select Site', SelectSites()),
+      SettingInfo(
+          Icons.email_outlined,
+          'Contact Us',
+          MatomeWebView(
+            title: "Contact Us",
+            selectedUrl: _settingScreen._contactURL,
+          )),
+      SettingInfo(
+          Icons.privacy_tip_outlined,
+          'Privacy Policy',
+          MatomeWebView(
+            title: "Privacy Policy",
+            selectedUrl: _settingScreen._PPURL,
+          )),
+    ];
+    return _settingScreen;
+  }
 
   @override
   Widget build(BuildContext context) {
-    //return MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      //title: "SETTING",
-      return Scaffold(
-        body: ListView.separated(
-          itemCount: 3,
-          separatorBuilder:
-              (BuildContext context, int index) => Divider(
-                  color: Colors.white),
-          itemBuilder: (BuildContext context, int index) {
-            Widget listtile;
-            switch (index) {
-              case 0:
-                listtile = ListTile(
-                  leading: Icon(Icons.select_all),
-                  title: Text(
-                    'Select Site',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () async {
-                    await Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => SelectSites()));
-                  },
-                );
-                break;
-              case 1:
-                listtile = ListTile(
-                  leading: Icon(Icons.email_outlined),
-                  title: Text(
-                    'Contact Us',
-                      style: TextStyle(fontSize: 20),
-                  ),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
-                    print(contactURL);
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            MatomeWebView(
-                              title: "Contact Us",
-                              selectedUrl: contactURL,
-                            )));
-                  },
-                );
-                break;
-              case 2:
-                listtile = ListTile(
-                  leading: Icon(Icons.privacy_tip_outlined),
-                  title: Text(
-                    'Privacy Policy',
-                      style: TextStyle(fontSize: 20),
-                  ),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
-                    print(PPURL);
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            MatomeWebView(
-                              title: "Privacy Policy",
-                              selectedUrl: PPURL,
-                            )));
-                  },
-                );
-                break;
-            }
-            return listtile;
-          },
-        ),
-      );
+    return Scaffold(
+      body: ListView.separated(
+        itemCount: _settingTabs.length,
+        separatorBuilder: (BuildContext context, int index) =>
+            Divider(color: Colors.white),
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(_settingTabs[index].icon),
+            title: Text(
+              _settingTabs[index].title,
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () async {
+              await Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => _settingTabs[index].widget));
+            },
+          );
+        },
+      ),
+    );
   }
 }
