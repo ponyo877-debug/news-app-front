755f13188e5d8e8e5935b4047cfb82fd9ac174d6
ponyo877-debug <hidakakis@nttdata.co.jp>
2021-05-11 11:21:09 +0900

    modify userAgent for iOS

--- lib/screens/webview.dart
+++ lib/screens/webview.dart
@@ -304,6 +304,10 @@ class _MatomeWebView extends State<MatomeWebView> {
     } on PlatformException {
       userAgent = '<error>';
     }
+    if (Platform.isIOS) {
+      userAgent =
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
+    }
     var response = await http.Client()
         .get(Uri.parse(loaduri), headers: {'User-Agent': userAgent});
     // print("response status: ${response.statusCode}");
